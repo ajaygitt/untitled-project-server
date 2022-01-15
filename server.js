7cf3ff54f68a37bdff039bc08dc573052bf7e443
ajaypradeep <ajaypradeep1712@gmail.com>
2022-01-15 22:30:48 +0530

    cors fix

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-let express =require("express") ;
-let cors =require('cors');
+let express = require("express");
+let cors = require("cors");
 let mongoose = require("mongoose");
-let fs =require("fs") ;
-const fileupload = require('express-fileupload')
+let fs = require("fs");
+const fileupload = require("express-fileupload");
 const morgan = require("morgan");
 // import cookieParser from "cookie-parser";
 const path = require("path");
 const app = express();
-require('dotenv').config()
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
 app.use(cookieParser());
-const usersRouter=require('./routes/user')
-const adminRouter=require('./routes/admin')
+const usersRouter = require("./routes/user");
+const adminRouter = require("./routes/admin");
 
-
-app.use(fileupload())
+app.use(fileupload());
 
 // connect DB
 // use this "process.env.LOCAL_MONGO_DB" connection string for local db
@@ -28,9 +27,9 @@ mongoose
   .then(() => console.log("MongoDB is connected"))
   .catch((err) => console.log("DB CONNECTION ERROR", err));
 
+  
 // apply middlewares
-app.use(cors({credentials: true, origin: process.env.FRONT_END_URL}));
-
+app.use(cors({ credentials: true, origin: process.env.FRONT_END_URL }));
 
 //serve static files
 app.use(express.static("images"));
@@ -39,9 +38,8 @@ app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("dev"));
 
-
-app.use('/', usersRouter);
-app.use('/admin', adminRouter);
+app.use("/", usersRouter);
+app.use("/admin", adminRouter);
 // // route - This func is for importing routes files automaticaly. so we dont need to import separately
 // fs.readdirSync("./routes").map((r) => app.use("/", require(`./routes/${r}`)));
 
